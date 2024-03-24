1a3921961b6dcfe427157606f4bf5c69d192ccfa
rishantkohli2033 <rishantkohli2033@gmail.com>
2024-03-25 03:39:30 +0530

    authentication completed (backend)

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,15 +1,22 @@
+"use client"
+import { auth } from '@/firebase/firebase';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { BsList } from 'react-icons/bs';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import Logout from '../Buttons/Logout';
+import { useSetRecoilState } from 'recoil';
+import { authModalState } from '@/atoms/authModalAtom';
 
 type TopbarProps = {
     
 };
 
 const Topbar:React.FC<TopbarProps> = () => {
-    
+    const [user] = useAuthState(auth);
+    const setAuthModalState = useSetRecoilState(authModalState);
     return (
         <nav className='relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7'>
             <div className={`flex w-full items-center justify-between max-w-[1200px] mx-auto`}>
@@ -18,19 +25,36 @@ const Topbar:React.FC<TopbarProps> = () => {
                 </Link>
 
                 <div className='flex items-center space-x-4 flex-1 justify-end'>
-                    <div>
-                        <a
-                            href='https://www.buymeacoffee.com/burakorkmezz'
-                            target='_blank'
-                            rel='noreferrer'
-                            className='bg-dark-fill-3 py-1.5 px-3 cursor-pointer rounded text-brand-orange hover:bg-dark-fill-2'
-                        >
-                            Premium
-                        </a>
-                    </div>
-                    <Link href='/auth'>
-                        <button className='bg-dark-fill-3 py-1 px-2 cursor-pointer rounded '>Sign In</button>
-                    </Link>
+                    
+                    {!user ? (
+                        <>
+                            <div>
+                                <Link
+                                    href='#'
+                                    target='_blank'
+                                    rel='noreferrer'
+                                    className='bg-dark-fill-3 py-1.5 px-3 cursor-pointer rounded text-brand-orange hover:bg-dark-fill-2'
+                                >
+                                    Premium
+                                </Link>
+                            </div>
+                            <Link href='/auth' onClick={()=>{setAuthModalState((prev)=>({...prev, isOpen: true, type: "login"}))}}>
+                                <button className='bg-dark-fill-3 py-1 px-2 cursor-pointer rounded '>Sign In</button>
+                            </Link>                                
+                        </>
+                    ) : (
+                        <>
+                            <div className='cursor-pointer group relative'>
+                                <img src="/avatar.png" alt="user profile img" className='h-8 w-8 round-full' />
+                                <div className='absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg z-40 group-hover:scale-100 scale-0 
+                                    transition-all duration-300 ease-in-out'
+                                >
+                                    <p className='text-sm'>{user.email}</p>
+                                </div>
+                            </div>
+                        <Logout/>  
+                       </>
+                    )}
                 </div>
             </div>
         </nav>
